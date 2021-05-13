591f2f4e6348edff10e498832a758439eccfc86b
victorgrappe <victor.grappe@protonmail.com>
2021-05-13 10:39:35 +0200

    Basic dots table

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 
-print("Hello omens !")
+import pandas
+import numpy
+
+
+def getDots():
+    dot_df = pandas.read_csv(filepath_or_buffer='./dots.csv')
+    dot_df = dot_df.drop(labels=['order'], axis=1)
+    return dot_df
+
+
+def run():
+    dot_df = getDots()
+    print(dot_df)
+
+
+if __name__ == '__main__':
+    run()
