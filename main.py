0f396dbff4f4635fd55b696c2bd6b386235d03c7
victorgrappe <victor.grappe@protonmail.com>
2021-05-13 11:33:01 +0200

    Set dtypes + summarize()

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 
+import os
 import pandas
 import numpy
 
 
 def getDots():
-    dot_df = pandas.read_csv(filepath_or_buffer='./dots.csv')
+    dot_df = pandas.read_csv(
+        filepath_or_buffer='./dots.csv',
+        dtype={
+            'order':    'object',
+            'key':      'object',
+            '<':        'object',
+            'astre':    'object',
+            'country':  'object',
+            'city':     'object',
+            'mass':     'float',
+            'utc':      'object'
+        },
+        parse_dates=['utc'],
+    )
     dot_df = dot_df.drop(labels=['order'], axis=1)
     return dot_df
 
 
+def clearScreen():
+   if os.name == 'posix':
+      os.system('clear')
+   else:
+      os.system('cls')
+
+
+def summarize(df):
+    clearScreen()
+    rows, columns = os.popen('stty size', 'r').read().split()
+    sep = '-' * int(columns)
+    print(sep)
+    print(df)
+    print(sep)
+    print(df.dtypes)
+    print(sep)
+    print('shape :', df.shape, 'size', df.size)
+    print(sep)
+
+
 def run():
     dot_df = getDots()
-    print(dot_df)
+    summarize(df=dot_df)
 
 
 if __name__ == '__main__':
